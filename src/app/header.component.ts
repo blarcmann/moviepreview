dddeeca2d164d4677d72045bd9122d62dde26478
yemiOdetola <yemiotola@outlook.com>
2018-12-14 09:48:47 +0100

    fix: on designs still

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -2,15 +2,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    template: `
-        <header>
-            <div class="row">
-                <div (click)="startNewSearch.emit()" class="col s4 offset-s4 search-btn center-align">
-                    <span class="material-icons">search</span>
-                </div>
-            </div>
-        </header>
-    `,
+    templateUrl: './header.component.html',
     styles: []
 })
 
